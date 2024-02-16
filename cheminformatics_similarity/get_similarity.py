47b0ad258434f2f33e9a95f2697de1a197df6067
Kevin Spiekermann <kspieker@mit.edu>
2024-02-16 17:20:52 -0500

    Use `DataStructs.BulkTanimotoSimilarity`

--- cheminformatics_similarity/get_similarity.py
+++ cheminformatics_similarity/get_similarity.py
@@ -15,29 +15,24 @@ def get_RDKit_fp(smi):
 	mol = Chem.MolFromSmiles(smi)
 	return fp_gen.GetCountFingerprint(mol)
 
-def get_sim(smi1, smiles_list):
-	sim_results = np.zeros(len(smiles_list))
-	for i, smi2 in enumerate(smiles_list):
-		fp1 = get_RDKit_fp(smi1)
-		fp2 = get_RDKit_fp(smi2)
+def get_sim(fp, fp_list):
+	
+	similarities = DataStructs.BulkTanimotoSimilarity(fp, fp_list)
 
-		# todo: explore DataStructs.BulkTanimotoSimilarity() as another way to do this
-		sim = DataStructs.TanimotoSimilarity(fp1, fp2)
-		sim_results[i] = sim
+	return similarities
 
-	return sim_results
 
-def get_similarity_matrix(smiles_list1, smiles_list2, ncpus=1):
+def get_similarity_matrix(fp_list1, fp_list2, ncpus=1):
 	"""
 	Returns a matrix of Tanimoto similarities as a numpy array
-	of size smiles_list1 x smiles_list2.
+	of size fp_list1 x fp_list2.
 
-	Progress is measured by the number of smiles in smiles_list1.
+	Progress is measured by the number of smiles in fp_list1.
 	"""
 
-	result = Parallel(n_jobs=ncpus, backend='multiprocessing', verbose=5)(delayed(get_sim)(smi, smiles_list2) for smi in smiles_list1)
+	result = Parallel(n_jobs=ncpus, backend='multiprocessing', verbose=5)(delayed(get_sim)(fp, fp_list2) for fp in fp_list1)
 
-	similarity_matrix = np.zeros((len(smiles_list1), len(smiles_list2)))
+	similarity_matrix = np.zeros((len(fp_list1), len(fp_list2)))
 	for i, res in enumerate(result):
 		similarity_matrix[i, :] = res
 
