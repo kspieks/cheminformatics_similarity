d6c4abcc47133eaca1d5e7f5cf406caf6e2046d9
Kevin Spiekermann <kspieker@mit.edu>
2026-02-28 17:39:19 -0800

    Remove pandarallel dependency

--- calculate_chemical_similarity.py
+++ calculate_chemical_similarity.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # encoding: utf-8
+
 import pickle as pkl
 
 import pandas as pd
-from pandarallel import pandarallel
+from joblib import Parallel, delayed
 
 from cheminformatics_similarity.get_similarity import get_RDKit_fp, get_similarity_matrix
 from cheminformatics_similarity.parsing import parse_cli_args
@@ -15,20 +16,23 @@ def main():
     """
 
     args = parse_cli_args()
-    pandarallel.initialize(nb_workers=args.n_cpus_featurize, progress_bar=True)
 
     df1 = pd.read_csv(args.data_path1)
     df2 = pd.read_csv(args.data_path2)
 
     # get list of rdkit.DataStructs.UIntSparseIntVect
-    df1['fp'] = df1.smiles.parallel_apply(get_RDKit_fp)
-    df2['fp'] = df2.smiles.parallel_apply(get_RDKit_fp)
+    print("Generating fingerprints...")
+    fp_list1 = Parallel(n_jobs=args.n_cpus_featurize, backend="loky", verbose=5)(
+        delayed(get_RDKit_fp)(smi) for smi in df1.smiles.tolist()
+    )
+    fp_list2 = Parallel(n_jobs=args.n_cpus_featurize, backend="loky", verbose=5)(
+        delayed(get_RDKit_fp)(smi) for smi in df2.smiles.tolist()
+    )
 
-    fp_list1 = list(df1.fp.values)
-    fp_list2 = list(df2.fp.values)
     print(f'fp_list1 has {len(fp_list1)} smiles')
     print(f'fp_list2 has {len(fp_list2)} smiles')
-    print('Progress is measured by the number of molecules in fp_list1')
+    print("Computing similarity matrix")
+    print('Progress is measured by the number of molecules in fp_list1...')
 
     similarity_matrix = get_similarity_matrix(fp_list1, fp_list2, ncpus=args.n_cpus)
     print(f'similarity_matrix.shape: {similarity_matrix.shape}')
